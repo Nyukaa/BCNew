64ac3253987734da92c9bd14c90dde86bb0f5798
Nyukaa <annashitikova@gmail.com>
2025-03-28 12:58:12 +0200

    Tehtävä 1 JavaScript

---
+++ Harjoitukset/viikko_1/kansio/script.js
@@ -0,0 +1,18 @@
+console.log("täma on Javascript-testi");
+//alert("Moi!");
+const muutaja = "Hello";
+console.log(typeof muutaja);
+console.log(instanceof );
+
+function funktio() {
+  let p = 5 * 6;
+  let x, y, z;
+  x = 5;
+  y = 6;
+  z = x + y;
+  console.log("Z: ", z);
+  let toinen = "arvo";
+  console.log(toinen);
+}
+//Kutsu functio näin
+funktio();
