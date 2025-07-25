bb2c05503d3cc3f00d25d33a873449da438ff700
Nyukaa <annashitikova@gmail.com>
2025-07-25 20:32:09 +0300

    FinalVerKesaTeh1

--- article-preview-component-master/script.js
+++ article-preview-component-master/script.js
@@ -1,7 +1,6 @@
 const shareButton = document.getElementById("shareButton");
 const popupMenu = document.getElementById("popupMenu");
 
-// Показать/скрыть при клике
 shareButton.addEventListener("click", (e) => {
   e.stopPropagation();
 
@@ -9,19 +8,16 @@ shareButton.addEventListener("click", (e) => {
 
   popupMenu.style.display = isVisible ? "none" : "flex";
 
-  // Меняем фон кнопки
   shareButton.style.backgroundColor = isVisible
     ? "hsl(210, 46%, 95%)"
     : "#48556a";
 
-  // Меняем цвет иконки внутри (если это svg path)
   const svgPath = shareButton.querySelector("path");
   if (svgPath) {
     svgPath.setAttribute("fill", isVisible ? "#6E8098" : "#ffffff");
   }
 });
 
-// Скрыть при клике вне
 document.addEventListener("click", () => {
   popupMenu.style.display = "none";
   shareButton.style.backgroundColor = "hsl(210, 46%, 95%)";
@@ -32,7 +28,6 @@ document.addEventListener("click", () => {
   }
 });
 
-// Чтобы клик внутри меню не закрывал его
 popupMenu.addEventListener("click", (e) => {
   e.stopPropagation();
 });
