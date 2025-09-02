c7d8116367731e15fbbfc9e263dca0985d6c84d6
Nyukaa <annashitikova@gmail.com>
2025-09-02 14:57:50 +0300

    react 2_12-14

---
+++ React/01-oppimistehtavat/04-part2/src/App.jsx
@@ -0,0 +1,99 @@
+import { useState, useEffect } from "react";
+// import axios from "axios";
+import Note from "./components/Note";
+import noteService from "./services/notes";
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+
+  // Загружаем заметки при первой отрисовке
+  useEffect(() => {
+    console.log("Fetching notes...");
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+      // .then((response) => {
+      //   setNotes(response.data);
+    });
+  }, []);
+
+  // Добавление новой заметки
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    };
+
+    // axios.post("http://localhost:3003/notes", noteObject).then((response) => {
+    //   console.log("New note added:", response.data);
+    //   setNotes(notes.concat(response.data));
+    //   setNewNote("");
+    // });
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      // .then((response) => {
+      //   setNotes(notes.concat(response.data));
+      setNewNote("");
+    });
+  };
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+  // const toggleImportanceOf = (id) => {
+  //   console.log(`importance of ${id} needs to be toggled`);
+  // };
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3003/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    // axios.put(url, changedNote).then((response) => {
+    //   setNotes(notes.map((note) => (note.id === id ? response.data : note)));
+    // });
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id === id ? returnedNote : note)));
+        // .then((response) => {
+        //   setNotes(notes.map((note) => (note.id === id ? response.data : note)));
+      })
+      .catch((error) => {
+        alert(`the note '${note.content}' was already deleted from server`);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
+  };
+  // Фильтрация заметок
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+
+      <ul>
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
---
+++ React/01-oppimistehtavat/04-part2/src/services/notes.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3003/notes";
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const create = (newObject) => {
+  const request = axios.post(baseUrl, newObject);
+  return request.then((response) => response.data);
+};
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
+
+export default {
+  getAll,
+  create,
+  update,
+  //   getAll: getAll,
+  //   create: create,
+  //   update: update,
+};
+// import axios from "axios";
+// const baseUrl = "http://localhost:3003/notes";
+
+// const getAll = () => {
+//   return axios.get(baseUrl);
+// };
+
+// const create = (newObject) => {
+//   return axios.post(baseUrl, newObject);
+// };
+
+// const update = (id, newObject) => {
+//   return axios.put(`${baseUrl}/${id}`, newObject);
+// };
+
+// export default {
+//   getAll: getAll,
+//   create: create,
+//   update: update,
+// };
