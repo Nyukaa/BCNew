58a1017c01d072ced9d8de03198545d052bbbeda
Nyukaa <annashitikova@gmail.com>
2025-10-07 14:26:59 +0300

    part_4 note and 4_1-2

---
+++ React/Liam_lessons/Axios_lesson/src/components/Wolt.jsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react"
+
+export const Wolt = () => {
+    async function fetchDishes() {
+        const apiResponse = awaite axios.get('/api/consumer-')
+    }}
+    useEffect(() => {
+        fetchDishes()
+    }
